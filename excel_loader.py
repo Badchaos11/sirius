7486f43d25c643238974ada8c70117b0462c8a09
Badchaos11 <undrk95@gmail.com>
2022-07-11 12:36:24 +0700

    functions added

--- excel_loader.py
+++ excel_loader.py
@@ -1,22 +1,29 @@
 import pandas as pd
+from datetime import datetime
 import pymysql.cursors
 
 
-connection = pymysql.connect(  # Соединение с БД
-    host="localhost",
-    port=3306,
-    user="badchaos",
-    password="pe0038900",
-    db="sirius"
-)
+def get_excell():
+    connection = pymysql.connect(  # Соединение с БД
+        host="localhost",
+        port=3306,
+        user="badchaos",
+        password="pe0038900",
+        db="sirius"
+    )
 
-with connection:
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM `employees_employee`"
-        cursor.execute(sql)
-        result = cursor.fetchall()  # Получение всех данных из таблицы
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `employees_employee`"
+            cursor.execute(sql)
+            result = cursor.fetchall()  # Получение всех данных из таблицы
 
-df = pd.DataFrame(result,  # Формирование датафрейма
-                  columns=["ID", "Филиал", "ФИО", "Должность", "Дата рождения", "Дата приема на работу", "Зарплата"])
-df.to_excel("Dump.xlsx", index=False)  # Сохраниение в excell
+    df = pd.DataFrame(result,  # Формирование датафрейма
+                      columns=["ID", "Филиал", "ФИО", "Должность", "Дата рождения",
+                               "Дата приема на работу", "Зарплата"])
+    df.to_excel(f"Dump-{datetime.strftime(datetime.today().date(), '%Y%m%d')}.xlsx",
+                index=False)  # Сохраниение в excell
+    print("Документ создан")
 
+
+get_excell()
--- gd_dumps_loader.py
+++ gd_dumps_loader.py
@@ -2,23 +2,25 @@ import gspread as gd
 import pymysql.cursors
 
 
-gc = gd.service_account("sirius-355912-ca31fefaf074.json")  # Подключение сервисного аккаунта
+def fill_gspread():
+    gc = gd.service_account("sirius-355912-ca31fefaf074.json")  # Подключение сервисного аккаунта
 
-worksheet = gc.open("Sirius_DB_Dumps").worksheet("Dumps")  # Открытие таблицы и листа
+    worksheet = gc.open("Sirius_DB_Dumps").worksheet("Dumps")  # Открытие таблицы и листа
 
-connection = pymysql.connect(  # Создание соединенияс БД
-    host="localhost",
-    port=3306,
-    user="badchaos",
-    password="pe0038900",
-    db="sirius",
-    cursorclass=pymysql.cursors.Cursor
-)
+    connection = pymysql.connect(  # Создание соединенияс БД
+        host="localhost",
+        port=3306,
+        user="badchaos",
+        password="pe0038900",
+        db="sirius",
+        cursorclass=pymysql.cursors.Cursor
+    )
 
-with connection:
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM `employees_employee`"
-        cursor.execute(sql)
-        result = cursor.fetchall()  # Получение всех данных из БД
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `employees_employee`"
+            cursor.execute(sql)
+            result = cursor.fetchall()  # Получение всех данных из БД
 
-worksheet.update("A2", list(result))  # Сохранение результатов в Google Sheets
+    worksheet.update("A2", list(result))  # Сохранение результатов в Google Sheets
+    print("Обновление документа завершено")
